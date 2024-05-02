e44c1df379d8e61f8bd67304976d3a9433f28fb5
Aman Roy <yadavaman7632@gmail.com>
2024-05-02 10:35:19 +0530

    v 1.0.0.1

--- routes/auth.js
+++ routes/auth.js
@@ -5,8 +5,4 @@ router.post("/signup",authController.signup)
 router.post("/signin",authController.signin)
 router.post("/forgot-password",authController.forgetPasswordCode)
 router.post("/reset-password",authController.resetPassword)
-router.get("/hello",(req,res,next)=>{
-    console.log("Say Hello");
-    res.json({code:200,message:"Response from server"})
-})
 module.exports = router
