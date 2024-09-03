f154e04e59c55d761a31a0c45f933bf66ba1eeae
Aman Roy <yadavaman7632@gmail.com>
2024-09-03 10:43:46 +0530

    added public route

--- app.js
+++ app.js
@@ -7,11 +7,15 @@ dotenv.config({
 })
 const connectFromMongoDb = require("./DB/conn.js");
 const errorHandler = require('./utils/ErrorHandler.js');
+const ApiResponse = require('./utils/ApiResponse.js');
 
 const app = express();
 // connectFromMongoDb().then((response)=>{
 
 // })
+app.get("/",(req,res)=>{
+    res.send( new ApiResponse(200,{blog:[{name:"John doe",age:34,gender:"male"}]},"Welcome to Blog Backend!"))
+})
 app.use(express.json({ limit: "500mb" }))
 app.use("/api/v1/auth", authRoute)
 app.use("/api/v1/user", postRoute)
